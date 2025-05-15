c925d139811333dc0826b4f7a32ea86d4cafa912
debidutta-quotus <debidutta.acharya@quotus.co.in>
2025-05-15 16:11:02 +0530

    refactor: remove unused route files for amenities, properties, rooms, and uploads

--- server/pms_api/src/routes/upload.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import upload from "../utils/multer";
-import { uploadHandler } from "../controller/upload.controller";
-
-const router = Router();
-
-router.route("/").post(upload.array("file"), uploadHandler as any);
-
-export default router;
