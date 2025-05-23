53650a3eb283c0e5513a7885ec99b828f1afe5ab
suryapratap-1 <suryapratap0765@gmail.com>
2025-05-23 17:03:49 +0530

    Add React dependency and implement getRatePlanByHotelCode method in RatePlan service and DAO

--- server/rate-plan/src/dao/ratePlan.dao.ts
+++ server/rate-plan/src/dao/ratePlan.dao.ts
@@ -2,20 +2,20 @@ import { RatePlan as RatePlanType } from "../common/interface/ratePlan.interface
 import RatePlan from "../model/ratePlan.model";
 
 class RatePlanDao {
-    public static async create(ratePlanData: RatePlanType) {
-      
-        try {
-             const ratePlan = new RatePlan(ratePlanData);
-    return await ratePlan.save();
-        }
-        catch (error) {
-            console.error("Error creating rate plan:", error);
-            throw new Error("Failed to create rate plan");
-        }
-   
+  public static async create(ratePlanData: RatePlanType) {
+
+    try {
+      const ratePlan = new RatePlan(ratePlanData);
+      return await ratePlan.save();
     }
-    
-    public static async updateRatePlan(id: string, data: any) {
+    catch (error) {
+      console.error("Error creating rate plan:", error);
+      throw new Error("Failed to create rate plan");
+    }
+
+  }
+
+  public static async updateRatePlan(id: string, data: any) {
     const updated = await RatePlan.findByIdAndUpdate(id, data, {
       new: true,
       runValidators: true,
@@ -28,6 +28,9 @@ class RatePlanDao {
     return updated;
   }
 
+  public static async getRatePlanByHotelCode(hotelCode: string) {
+    return await RatePlan.find({ hotelCode });
+  }
 
 }
 
