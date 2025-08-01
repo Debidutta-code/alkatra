339c26ad25a4d8b5c5d2f067f5e335ceb86358b2
suryapratap-1 <suryapratap0765@gmail.com>
2025-08-01 15:08:59 +0530

    Update app.ts by removing hardcoded values with environment variable's

--- server/src/config/env.variable.ts
+++ server/src/config/env.variable.ts
@@ -6,7 +6,38 @@ const { env } = process;
 
 export const config = {
 
+    server: {
+        port: Number(env.PORT),
+        host: env.HOST as string,
+        mode: env.MODE as string,
+        expressJsonLimit: env.EXPRESS_JSON_LIMIT as string,
+        expressUrlencodedLimit: env.EXPRESS_URLENCODED_LIMIT as string,
+        expressStaticFolderPath: env.EXPRESS_STATIC_FOLDER_PATH as string,
+        expressUrlencodedExtended: Boolean(env.EXPRESS_URLENCODED_EXTENDED),
+        morganMode: env.MORGAN_MODE as string,
+        appName: env.APP_NAME as string,
+        baseUrl: env.BASE_URL as string,
+        deepLink: env.DEEP_LINK as string,
+        timeZone: env.TIMEZONE as string,
+        cors: {
+            origins: env.CORS_ORIGINS.split(",") as string[],
+            methods: env.CORS_METHODS.split(",") as string[],
+            allowedHeaders: env.CORS_HEADERS.split(",") as string[],
+            credentials: Boolean(env.CORS_CREDENTIALS)
+        },
+        jwt: {
+            secretKey: env.JWT_SECRET_KEY as string,
+            expiresIn: env.JWT_EXPIRES_IN as string,
+            devSecretKey: env.JWT_SECRET_KEY_DEV as string,
+            devExpiresIn: env.JWT_EXPIRES_IN_DEV as string
+        },
+    },
+
     companyName: env.COMPANY_NAME as string,
+
+    database: {
+        mongoURI: env.EXTRANET_MONGO_URI_TESTING as string
+    },
 
     // AWS config
     aws: {
